79d3fabf724c65c85073068a1f8634ff5142e698
GoodPBC <jpulaski79@gmail.com>
2016-09-28 17:15:03 -0400

    heroku deploy

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -12,103 +12,9 @@
 //=======================================================================site background===================================================================
 //=========================================================================================================================================================
 $( document ).ready(function() {
- console.log('test');
   $.backstretch("https://il6.picdn.net/shutterstock/videos/7080982/thumb/1.jpg");
   $("#top").backstretch("http://gaudiyadarshan.com/gaudiyadarshan.com/wp-content/uploads/2011/05/NirmalBhakti-Gradient-Background-light-blue-to-white.jpg");
 });
-//=========================================================================================================================================================
-//=======================================================================YOUTBE PLAYER API --sets up player--=====================================
-//=========================================================================================================================================================
-
-// 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "http://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '225',
-          width: '400',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-
-
-
-
-//=========================================================================================================================================================
-//=======================================================================YOUTBE DATA API --fed artist and track name--=====================================
-//=========================================================================================================================================================
-
-// https://developers.google.com/youtube/terms
-
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-   var responseString = JSON.stringify(response, '', 2);
-   console.log(responseString);
-   document.getElementById('response').innerHTML += responseString;
-}
-
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-   gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-   // This API key is intended for use only in this lesson.
-   // See http://goo.gl/PdPA1 to get a key for your own applications.
-   gapi.client.setApiKey('AIzaSyC5AZQrtUO4D7no_zKQvqyUNcNJ8cVnkOI');
-
-   search();
-}
-
-function search() {
-   // Use the JavaScript client library to create a search.list() API call.
-   var request = gapi.client.youtube.search.list({
-       part: 'snippet',
-       q: 'funny cats'
-       
-   });
-   
-   // Send the request to the API server,
-   // and invoke onSearchRepsonse() with the response.
-   request.execute(onSearchResponse);
-}
-
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-   showResponse(response);
-}
-
 
 //=================================================================================================================================
 //=======================================Artist Info Musicovery API================================================================
@@ -116,9 +22,9 @@ function onSearchResponse(response) {
 	var authKey = "&format=json&apikey=l0x48hv1";
 	var queryURLBase = "http://musicovery.com/api/V3/playlist.php?&listenercountry=us&resultsnumber=5&fct=getfrommood";
 	var enregyParam = "&trackvalence=";
-	var userEnergy = 100000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
+	var userEnergy = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
 	var moodParam = "&trackarousal=";
-	var userMood = 100000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
+	var userMood = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
 	userDecade = 50;	//ten needs to change with user age.  just hard coded for testing 
 	var decade = "&date" + userDecade + "=" + true;
 
@@ -147,3 +53,34 @@ function onSearchResponse(response) {
 			console.log(artistName);
 			console.log(artistTitle);	
 	});
+
+
+//=========================================================================================================================================================
+//=======================================================================YOUTBE DATA API --fed artist and track name--=====================================
+//=========================================================================================================================================================
+$ (function() {
+	$("form").on("submit", function(e){
+		e.preventDefault();
+		//prepare the request
+		var request = gapi.client.search.list({
+					part: "snippet",
+					type: "video",
+					q: "cat"
+		});
+		request.execute(function(response) {
+			console.log(response);
+		});
+	});
+});
+
+
+function init() {
+	gapi.client.setApiKey("AIzaSyC5AZQrtUO4D7no_zKQvqyUNcNJ8cVnkOI");
+	gapi.client.load("youtube", "v3", function() {
+		//YT api is ready
+		console.log('test');
+	});
+}
+
+
+
