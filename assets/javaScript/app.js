7e29f6a22af054447c7bb33862095219c1e6d582
Anthony Delgado <me@anthonydelgado.me>
2016-09-30 03:24:52 -0400

    clean up code and add iframe player

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,5 +1,3 @@
-
-
 $( document ).ready(function() {
 
 $("#begin").click(function() {
@@ -47,136 +45,40 @@ $("#submitSad").click(function() {
 	var authKey = "&format=json&apikey=l0x48hv1";
 	var queryURLBase = "https://crossorigin.me/http://musicovery.com/api/V3/playlist.php?&listenercountry=us&resultsnumber=5&fct=getfrommood";
 	var enregyParam = "&trackvalence=";
-	var userEnergy = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
+	var userEnergy = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing
 	var moodParam = "&trackarousal=";
-	var userMood = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing 
+	var userMood = 900000; //scale of 100000 - 900000 will have to change with user input/ user score  just hard coded for testing
 	userDecade = 50;	//ten needs to change with user age.  just hard coded for testing 
 	var decade = "&date" + userDecade + "=" + true;
 
-
-
-	console.log(queryURLBase);
-	console.log(authKey);
-	console.log(enregyParam);
-	console.log(moodParam);
-	console.log(decade);
 	//URL for ajax call 
-	queryURL = queryURLBase + enregyParam + userEnergy + moodParam + userMood + decade + userDecade + authKey
-	console.log(queryURL);
+	queryURL = queryURLBase + enregyParam + userEnergy + moodParam + userMood + decade + userDecade + authKey ;
 
 
 
 	$.ajax({url: queryURL, method: "GET"}) 
-		.done(function(root) {
+		.done(function(response) {
 
-			console.log("------------------------------------");
-			console.log("URL: " + queryURL);
-			console.log("------------------------------------");
 
-			var artistName = root.root.tracks.track[0].artist.name;
-			var artistTitle = root.root.tracks.track[0].title;
+			var artistName = response.root.tracks.track[0].artist.name;
+			var artistTitle = response.root.tracks.track[0].title;
 			
 			artistName = artistName.replace(" ","+");
 			artistTitle = artistTitle.replace(" ", "+");	
 
-			var queryURL_YT = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + artistName + artistTitle + "&type=video&fields=items%2Fid%2FvideoId&key=AIzaSyBwVDM-Vd_i_HMVlPJXFbBW0lmZSjf_h2s";
-				console.log(queryURL_YT);
+			var queryURL_YT = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + artistName + artistTitle + "&type=video&key=AIzaSyBwVDM-Vd_i_HMVlPJXFbBW0lmZSjf_h2s";
+
+
 			$.ajax({url: queryURL_YT, method: "GET"}) 
 		.done(function(response) {
 				var ytVideoId = response.items[0].id.videoId; // this gets fed to youtube embed
-				
-				console.log(root.root);
+
 				console.log(artistName);
-				console.log(artistTitle);
+                console.log(artistTitle);
+            $('#player').attr('src', '//www.youtube.com/embed/' + ytVideoId + '?rel=0&amp;autoplay=1')
 		});
 	});
 
 
-//=========================================================================================================================================================
-//=======================================================================YOUTBE DATA API --fed artist and track name--=====================================
-//=========================================================================================================================================================
 
-// 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '225',
-          width: '400',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-
-
-// https://developers.google.com/youtube/terms
-
-// Helper function to display JavaScript value on HTML page.
-// function showResponse(response) {
-//    var responseString = JSON.stringify(response, '', 2);
-//    console.log(responseString);
-//    document.getElementById('#player').innerHTML += responseString;
-// }
-
-// // Called automatically when JavaScript client library is loaded.
-// function onClientLoad() {
-//    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-// }
-
-// // Called automatically when YouTube API interface is loaded (see line 9).
-// function onYouTubeApiLoad() {
-//    // This API key is intended for use only in this lesson.
-//    // See http://goo.gl/PdPA1 to get a key for your own applications.
-//    gapi.client.setApiKey('AIzaSyC5AZQrtUO4D7no_zKQvqyUNcNJ8cVnkOI');
-
-//    search();
-// }
-
-// function search() {
-//    // Use the JavaScript client library to create a search.list() API call.
-//    var request = gapi.client.youtube.search.list({
-//        part: 'snippet',
-//        q: 'funny cats'
-       
-//    });
-   
-//    // Send the request to the API server,
-//    // and invoke onSearchRepsonse() with the response.
-//    request.execute(onSearchResponse);
-// }
-
-// // Called automatically with the response of the YouTube API request.
-// function onSearchResponse(response) {
-//    showResponse(response);
-
-// }
 });
